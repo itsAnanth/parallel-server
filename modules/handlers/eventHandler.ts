d4554573bbc32515bfb4dd8bede618b42c98e8ff
EJBEAN2op <danonymousarmy@gmail.com>
2022-02-20 20:26:01 +0530

    fix(shared): event handler paths

--- modules/handlers/eventHandler.ts
+++ modules/handlers/eventHandler.ts
@@ -1,11 +1,12 @@
 import fs from 'fs';
 import { Client } from 'discord.js';
+import type { Event } from '../../shared/types/Event';
 
-async function handleEvents(bot: Client, dir: { absolutePath: string, path: string }) {
-    const eventsFolder = fs.readdirSync(dir.absolutePath).filter(x => x.endsWith('js') && x != 'index.js');
+async function handleEvents(bot: Client, dir: { absolutePath: string, path: string, name: string }) {
+    const eventsFolder = fs.readdirSync(dir.absolutePath).filter(x => x.endsWith('.ts'));
     for (const file of eventsFolder) {
-        let event = await import(`${dir.path}/${file}`);
-        event = event.default;
+        let _event = await import(`../../core/${dir.name}/events/${file}`);
+        let event: Event = _event.default;
         bot.on(event.name.toString(), event.execute.bind(null, bot));
         console.log('[event handler]', `${event.name}`);
     }
