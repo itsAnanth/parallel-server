aa4a6838fe741c043a2f8fd7d22884e8a4efa2ba
EJBEAN2op <danonymousarmy@gmail.com>
2022-02-20 19:13:00 +0530

    feat(shared): command typings

---
+++ shared/types/Command.ts
@@ -0,0 +1,36 @@
+import { Message, Client } from 'discord.js';
+
+type allowPayload = {
+    permissions?: number[];
+    roles?: string[];
+    ids?: string[]
+}
+
+type allow = {
+    permissions: number[];
+    roles: string[];
+    ids: string[];
+}
+
+interface CommandPayload {
+    name: string;
+    description?: string;
+    expectedArgs?: string;
+    cooldown?: number;
+    aliases?: string[];
+    allow?: allowPayload;
+    execute: (message: Message, args: string[], bot: Client) => Promise<void>;
+}
+
+interface Command {
+    name: string;
+    description: string;
+    expectedArgs: string;
+    cooldown: number;
+    aliases: string[];
+    allow: allow;
+
+    execute: (message: Message, args: string[], bot: Client) => Promise<void>;
+}
+
+export type { Command, allow, CommandPayload, allowPayload }
