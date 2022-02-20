80122b3e84c52037472ec9e48ec8261b263e2904
EJBEAN2op <danonymousarmy@gmail.com>
2022-02-20 20:25:22 +0530

    fix(shared): commands

--- modules/Command.ts
+++ modules/Command.ts
@@ -18,21 +18,21 @@ class Command {
     }
 
     run(message: IMessage, args: string[], bot: Client) {
-        if (!this.checkBotPermission(message))
-            return message.replyEmbed({
-                description: `Missing Bot Permission(s) | ${this.required.reduce((a, c) => new Permissions(c).toArray().concat(a), []).join(', ').trim()}`
-            });
+        // if (this.required.length != 0 && !this.checkBotPermission(message))
+        //     return message.replyEmbed({
+        //         description: `Missing Bot Permission(s) | ${this.required.reduce((a, c) => new Permissions(c).toArray().concat(a), []).join(', ').trim()}`
+        //     });
         
         this.execute(message, args, bot);
     }
 
-    checkBotPermission(message: IMessage) {
-        for (let i = 0; i < this.required.length; i++) {
-            if (!message.guild.me.permissions.has(this.required[i]))
-                return false;
-        }
-        return true;
-    }
+    // checkBotPermission(message: IMessage) {
+    //     for (let i = 0; i < this.required.length; i++) {
+    //         if (!message.guild.me.permissions.has(this.required[i]))
+    //             return false;
+    //     }
+    //     return true;
+    // }
 
     configBotPermissions(permissionArray: bigint[] | undefined) {
         this.required = permissionArray ? permissionArray : [];
