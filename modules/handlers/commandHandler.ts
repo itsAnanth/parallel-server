b01847a85c1c53749a1206aec03543b4b2924a24
EJBEAN2op <danonymousarmy@gmail.com>
2022-02-20 20:26:32 +0530

    fix(shared): command handler paths

--- modules/handlers/commandHandler.ts
+++ modules/handlers/commandHandler.ts
@@ -5,15 +5,16 @@ interface Client extends dClient {
     commands: Collection<string, any>;
 }
 
-async function handleCommands(bot: dClient, dir: { absolutePath: string, path: string }) {
-    let command = new Collection();
-    Object.assign(bot, command);
-
+async function handleCommands(bot: dClient, dir: { absolutePath: string, path: string, name: string }) {
+    let commands = new Collection();
+    
+    // @ts-ignore
+    bot.commands = commands;
     const commandFolders = fs.readdirSync(dir.absolutePath);
     for (const folder of commandFolders) {
-        const commandFiles = fs.readdirSync(`${dir}/${folder}`).filter(file => file.endsWith('.js'));
+        const commandFiles = fs.readdirSync(`${dir.absolutePath}/${folder}`).filter(file => file.endsWith('.ts'));
         for (const file of commandFiles) {
-            let command = await import(`${dir.path}/${folder}/${file}`);
+            let command = await import(`../../core/${dir.name}/commands/${folder}/${file}`);
             command = command.default;
             command.module = folder;
             (bot as Client).commands.set(command.name, command);
