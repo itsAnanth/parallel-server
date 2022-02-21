35e78bbd5e00ba077ad938798b084665704ab20a
EJBEAN2op <danonymousarmy@gmail.com>
2022-02-21 13:30:54 +0530

    chore(gfxbot): test command

--- core/gfxbot/commands/test/test.ts
+++ core/gfxbot/commands/test/test.ts
@@ -2,6 +2,7 @@ import Command from "../../../../modules/Command";
 
 export default new Command({
     name: 'test',
+    cooldown: 5,
     execute: async(message) => {
         message.channel.send('test');
     }
